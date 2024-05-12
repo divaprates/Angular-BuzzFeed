521dad30c35e93d24ec21a9cbf738a51afe88998
divaprates <divasilvaprates@gmail.com>
2024-05-11 23:24:51 -0300

    feat: Calculando o resultado

--- src/app/components/quizz/quizz.component.ts
+++ src/app/components/quizz/quizz.component.ts
@@ -34,6 +34,7 @@ export class QuizzComponent implements OnInit{
 
   playerChoose(value: string) {
     this.answers.push(value);
+    this.nextStep();
   }
 
   async nextStep() {
@@ -42,8 +43,20 @@ export class QuizzComponent implements OnInit{
     if (this.questionMaxIndex > this.questionIndex) {
       this.questionSelected = this.questions[this.questionIndex];
     } else {
+      const finalAnswer: string = await this.checkResult(this.answers);
       this.finished = true;
+      this.answerSelected = quizz_questions.results[finalAnswer as keyof typeof quizz_questions.results];
     }
   }
 
+  async checkResult(anwsers: string[]) {
+    const result = anwsers.reduce((previous, current, i, arr) => {
+      if(arr.filter(item => item === previous).length > arr.filter(item => item === current).length) {
+        return previous;
+      } else {
+        return current;
+      }
+    });
+    return result;
+  }
 }
